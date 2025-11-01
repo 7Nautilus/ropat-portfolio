4521a59bb83317e21bf094be7ceb1f100b6b7bad
7Nautilus <totor040403@gmail.com>
2025-11-01 15:22:29 +0100

    test animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,7 +192,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       }, {
         threshold: 0,
-        rootMargin: '20% 0px -25% 0px'
+        rootMargin: '100% 0px -100% 0px'
       });
 
       animatedBlocks.forEach(element => observer.observe(element));
